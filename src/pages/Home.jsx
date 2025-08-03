b2f875f74b12084e83c73b54677340e9bd0d230b
kashishgupta29 <119280832+kashishgupta29@users.noreply.github.com>
2025-08-03 18:02:38 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
+import Footer from '../components/Footer'
+import Plan from '../components/Plan'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <AiTools/>
+      <Testimonial/>
+      <Plan/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
