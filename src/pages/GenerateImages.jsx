2c35b42de84bd7c78a92deaf26fdab134ab1e4ae
kashishgupta29 <kashish.kg11@gmail.com>
2025-08-06 00:11:10 +0530

    completed generate-images page

--- src/pages/GenerateImages.jsx
+++ src/pages/GenerateImages.jsx
@@ -26,7 +26,19 @@ const GenerateImages = () => {
              'bg-purple-50 text-purple-700':'text-gray-500 border-gray-300'}`} key ={item}> {item}</span>
           ))}
         </div>
-        <br/>
+        <div className='my-6 flex items-center gap-2'>
+          <label className='relative cursor-pointer'>
+            <input type="checkbox" onChange={(e)=>setPublish(e.target.checked)} checked={publish} className='sr-only peer' />
+            <div className='w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition'>
+            </div>
+            <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-4'>
+
+            </span>
+          </label>
+          <p className='text-sm'>Make this image Public</p>
+
+        </div>
+
         <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
           <Image className='w-5'/>
           Generate Image
